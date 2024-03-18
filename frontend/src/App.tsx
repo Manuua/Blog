47aca5fd4adb4cecd8b6a6d2e6adb4cffebefeb2
Manoela Andrade <manoelaandrade@MacBook-Air-de-Manoela.local>
2024-03-17 21:42:15 -0300

    feat: remove unecessary fles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import Home from './Pages/Home';
-import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div> 
-      
         <Home />
     </div>
   );
